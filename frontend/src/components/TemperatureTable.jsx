660532bacd22f513047a725f09b1d5ac1de11251
Ossi Ruokokoski <ossi.posti@gmail.com>
2020-03-09 16:27:09 +0200

    Added humidity to the list

--- frontend/src/components/TemperatureTable.jsx
+++ frontend/src/components/TemperatureTable.jsx
@@ -9,12 +9,14 @@ export const Table = (props) => {
                     <th>Id</th>
                     <th>Timestamp</th>
                     <th>Temperature</th>
+                    <th>Humidity</th>
                 </tr>
 
                 {props.events.map((event) => (<tr>
                     <td>{event.id}</td>
                     <td>{event.timestamp}</td>
                     <td>{event.temperature}</td>
+                    <td>{event.humidity}</td>
                 </tr>))}
             </table>
     </div>
